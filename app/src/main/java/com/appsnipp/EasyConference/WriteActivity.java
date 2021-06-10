c87e8dba04771393015b455dfc81efe889aa0173
dlwpdnjs <kangmk0800@naver.com>
2021-06-10 19:02:03 +0900

    상세 화면 추가 진행중

--- app/src/main/java/com/appsnipp/EasyConference/WriteActivity.java
+++ app/src/main/java/com/appsnipp/EasyConference/WriteActivity.java
@@ -5,6 +5,13 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class WriteActivity extends AppCompatActivity {
 
@@ -22,6 +29,15 @@ public class WriteActivity extends AppCompatActivity {
         DateBaseHelper dbHelper = new DateBaseHelper(WriteActivity.this, "Conference.db", null, 1);
         sqliteDatabase = dbHelper.getWritableDatabase();
         dbHelper.onCreate(sqliteDatabase);
+
+        //현재 날짜 표출
+        Date Date = Calendar.getInstance().getTime();
+        String currentDate = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(Date);
+
+        EditText CurrentDate = (EditText) findViewById(R.id.regdate);
+        CurrentDate.setText(currentDate);
+        CurrentDate.setClickable(false);
+        CurrentDate.setFocusable(false);
 
         //뒤로가기 메소드 호출
         backKeyClickHandler = new BackKeyClickHandler(WriteActivity.this);
